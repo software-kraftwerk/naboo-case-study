265d250ec3e8ca8c86bda57cee735a86ead0f979
Julien Viala <julien.viala@shine.fr>
2026-04-01 21:14:10 +0200

    fix: obfuscate login failure reason

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  HttpException,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/user.schema';
@@ -18,11 +14,17 @@ export class AuthService {
   ) {}
 
   async signIn({ email, password }: SignInInput): Promise<SignInDto> {
-    const user = await this.userService.getByEmail(email);
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Wrong credentials provided');
+    }
+
     const isSamePassword = await bcrypt.compare(password, user.password);
 
-    if (!isSamePassword)
-      throw new HttpException('Wrong credentials provided', 400);
+    if (!isSamePassword) {
+      throw new UnauthorizedException('Wrong credentials provided');
+    }
 
     const token = await this.generateToken({ user });
 
