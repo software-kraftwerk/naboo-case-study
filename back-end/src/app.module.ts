2460e3a3ae35a8c265988126935328639fe62b1b
Julien Viala <julien.viala@shine.fr>
2026-04-01 21:48:04 +0200

    fix: dont throw if expired jwt

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, UnauthorizedException } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ActivityModule } from './activity/activity.module';
@@ -42,8 +42,8 @@ import { appConfig } from './config/app.config';
                 jwtPayload = (await jwtService.verifyAsync(token, {
                   secret,
                 })) as PayloadDto;
-              } catch (error) {
-                throw new UnauthorizedException(error);
+              } catch {
+                jwtPayload = null;
               }
             }
 
